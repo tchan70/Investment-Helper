2e75f91c0d5e90cca3e2a2bfff412c4863a04f0e
Toby Chan <toby70@hotmail.co.uk>
2024-06-07 01:22:56 +0100

    added a .env.example also added a default value for stock prices

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Stock(models.Model):
     symbol = models.CharField(max_length=10)
     companyName = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, default=0, decimal_places=2)
 
     def __str__(self):
         return self.name
